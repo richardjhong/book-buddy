2fbcf6071735ab634241bc0efe998a1e22037370
Richard Hong <rhong24@gmail.com>
2023-05-11 12:36:46 -0500

    creating chat input, using accordion from shadcn/ui, adding tailwind extensions for keyframes and animations to make accordion expansion visually more pleasing

---
+++ src/components/ChatInput.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { cn } from '@/lib/utils';
+import { FC, HTMLAttributes, useState } from 'react';
+import TextareaAutosize from 'react-textarea-autosize';
+
+interface ChatInputProps extends HTMLAttributes<HTMLDivElement> {
+  
+}
+
+const ChatInput: FC<ChatInputProps> = ({ className, ...props }) => {
+  const [input, setInput] = useState<string>('');
+  return (
+    <div {...props} className={cn('border-t border-zinc-300', className)}>
+      <div className="relative mt-4 flex-1 overflow-hidden rounded-lg border-none outline-none">
+        <TextareaAutosize 
+          rows={2}
+          maxRows={4}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          autoFocus
+          placeholder="Write a prompt..."
+          className='peer disabled:opacity-50 pr-14 resize-none block w-full border-0 bg-zinc-200 py-1.5 text-gray-900 focus:ring-0 text-sm sm:leading-6'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default ChatInput
